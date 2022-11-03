b5afc3add404c02fb729bbba35a0cc5f50aef289
leonarderx <92988489+leonarderx@users.noreply.github.com>
2022-11-03 18:44:42 +0300

    Add files via upload

---
+++ SuperPuuuupeeeeeeer.cpp
@@ -0,0 +1,76 @@
+/*#include <iostream>    
+using namespace std;
+
+
+/*
+int main() {
+
+	int arr[10];
+	for (int i = 0; i <= 10) {
+		cin << arr[i];
+
+	}
+
+	int arr[10];
+	for (int i = 0; i <= 10; i+=2) {
+		cout << arr[i] << ' ';         
+		*/
+//Cam1
+/*int colvo;
+
+int main() {
+
+	int arr[10];
+	for (int i = 0; i < 10; i++) {
+		cin >> arr[i];
+
+	}
+
+	for (int i = 0; i < 10; i++) {
+		if (arr[i] > 0) {
+			colvo++;
+		}
+	}
+	cout << colvo;
+}
+*/
+
+
+//Cam2
+/*int chet;
+int nechet;
+int main() {
+	int arr[10];
+	for (int i = 0; i < 10; i++) {
+		cin >> arr[i];
+
+	}
+	for (int i = 0; i < 10; i++) {
+		if (arr[i] % 2 == 0) {
+			chet++;
+		}
+		else {
+			nechet++;
+
+		}
+
+		
+	}
+	cout << chet << ' ' << nechet << endl;
+
+}*/
+
+//Cam3
+//3
+	/*int arr[10];
+	int sum = 0;
+	for (int i = 0; i < 10; i++) {
+		cin >> arr[i];
+	}
+	for (int i = 0; i < 10; i++) {
+		if (arr[i] % 3 != 0) {
+			sum = sum + arr[i];
+		}
+	}
+	cout << sum;*/
+*/
